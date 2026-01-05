1f6fe0f54a4f2842b1ef53953edfb7e60283bf81
MrESB <mr.abolfazlbeigi@gmail.com>
2026-01-05 23:10:48 +0330

    Implements Hall CRUD handlers and queries

---
+++ src/CinemaTicket.Application/Features/Halls/Queries/GetHallById/GetHallByIdQueryValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace CinemaTicket.Application.Features.Halls.Queries.GetHallById;
+
+public sealed class GetHallByIdQueryValidator : AbstractValidator<GetHallByIdQuery>
+{
+    public GetHallByIdQueryValidator()
+    {
+        RuleFor(x => x.Id).NotEmpty();
+    }
+}
