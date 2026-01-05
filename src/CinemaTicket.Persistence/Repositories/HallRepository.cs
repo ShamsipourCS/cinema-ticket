22d2710bcba73af0aa37b006cbebed9d27911d4a
MrESB <mr.abolfazlbeigi@gmail.com>
2026-01-05 22:46:06 +0330

    Add HallRepository and IHallRepository

---
+++ src/CinemaTicket.Persistence/Repositories/HallRepository.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using CinemaTicket.Application.Common.Interfaces;
+using CinemaTicket.Domain.Entities;
+using CinemaTicket.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaTicket.Persistence.Repositories;
+
+public sealed class HallRepository : IHallRepository
+{
+    private readonly ApplicationDbContext _context;
+
+    public HallRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Hall?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        => await _context.Halls.FirstOrDefaultAsync(h => h.Id == id, cancellationToken);
+
+    public async Task<IEnumerable<Hall>> GetAllAsync(CancellationToken cancellationToken = default)
+        => await _context.Halls.AsNoTracking().ToListAsync(cancellationToken);
+
+    public async Task<IEnumerable<Hall>> FindAsync(Expression<Func<Hall, bool>> predicate, CancellationToken cancellationToken = default)
+        => await _context.Halls.AsNoTracking().Where(predicate).ToListAsync(cancellationToken);
+
+    public async Task AddAsync(Hall entity, CancellationToken cancellationToken = default)
+        => await _context.Halls.AddAsync(entity, cancellationToken);
+
+    public Task UpdateAsync(Hall entity, CancellationToken cancellationToken = default)
+    {
+        _context.Halls.Update(entity);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(Hall entity, CancellationToken cancellationToken = default)
+    {
+        _context.Halls.Remove(entity);
+        return Task.CompletedTask;
+    }
+}
