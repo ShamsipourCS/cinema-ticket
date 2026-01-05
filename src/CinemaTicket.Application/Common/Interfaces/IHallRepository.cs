88d6050900108420a53f0badd07792cef959e37b
MrESB <mr.abolfazlbeigi@gmail.com>
2026-01-05 22:46:06 +0330

    Add HallRepository and IHallRepository

---
+++ src/CinemaTicket.Application/Common/Interfaces/IHallRepository.cs
@@ -0,0 +1,13 @@
+﻿using CinemaTicket.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicket.Application.Common.Interfaces
+{
+    public interface IHallRepository : IRepository<Hall>
+    {
+    }
+}
