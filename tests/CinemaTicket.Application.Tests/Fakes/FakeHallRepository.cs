4e9cad2a67de51d1ad362917cdffa327e9ee0f48
MrESB <mr.abolfazlbeigi@gmail.com>
2026-01-08 10:21:31 +0330

    refines FakeHallRepository

--- tests/CinemaTicket.Application.Tests/Fakes/FakeHallRepository.cs
+++ tests/CinemaTicket.Application.Tests/Fakes/FakeHallRepository.cs
@@ -1,4 +1,9 @@
-﻿using System.Linq.Expressions;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading;
+using System.Threading.Tasks;
 using CinemaTicket.Application.Common.Interfaces;
 using CinemaTicket.Domain.Entities;
 
@@ -7,6 +12,10 @@ namespace CinemaTicket.Application.Tests.Fakes;
 public sealed class FakeHallRepository : IHallRepository
 {
     private readonly List<Hall> _store = new();
+
+    public int AddCalls { get; private set; }
+    public int UpdateCalls { get; private set; }
+    public int DeleteCalls { get; private set; }
 
     public void Seed(params Hall[] halls) => _store.AddRange(halls);
 
@@ -19,27 +28,37 @@ public sealed class FakeHallRepository : IHallRepository
     public Task<IEnumerable<Hall>> FindAsync(Expression<Func<Hall, bool>> predicate, CancellationToken cancellationToken = default)
     {
         var compiled = predicate.Compile();
-        var result = _store.Where(compiled).ToList();
-        return Task.FromResult<IEnumerable<Hall>>(result);
+        return Task.FromResult<IEnumerable<Hall>>(_store.Where(compiled).ToList());
     }
 
     public Task AddAsync(Hall entity, CancellationToken cancellationToken = default)
     {
+        AddCalls++;
         _store.Add(entity);
         return Task.CompletedTask;
     }
 
     public Task UpdateAsync(Hall entity, CancellationToken cancellationToken = default)
     {
+        UpdateCalls++;
+
         var index = _store.FindIndex(h => h.Id == entity.Id);
         if (index >= 0)
+        {
             _store[index] = entity;
+        }
+        else
+        {
+            // In-memory behavior: if not found, treat update as upsert
+            _store.Add(entity);
+        }
 
         return Task.CompletedTask;
     }
 
     public Task DeleteAsync(Hall entity, CancellationToken cancellationToken = default)
     {
+        DeleteCalls++;
         _store.RemoveAll(h => h.Id == entity.Id);
         return Task.CompletedTask;
     }
